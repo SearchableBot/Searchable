63511174de409edbc7d6ab2bdf3b0a7523ead472
James <85806019+James-Discord@users.noreply.github.com>
2021-08-21 14:46:43 +0100

    Update search.js

--- commands/search.js
+++ commands/search.js
@@ -1,11 +1,12 @@
 exports.run = async (client, message, args) => {
+    let screenshot = await page.screenshot({type: 'png'});
     let m = await message.channel.send('Opening...')
     if(args[0].includes("file://"))
         return message.channel.send("This link is blacklisted!") 
     try {
         args[0] = args[0].substr(0,4)=="http"?args[0]: `http://${args[0]}`
         await page.goto(args[0])
-        m.edit(`:white_check_mark:  Opened ${args[0]}`)
+        m.edit(`:white_check_mark:  Opened ${args[0]} ({files: [{attachment: screenshot, name: "screenshot.png"}]})`)
     } catch (e) {
         m.edit(`:x: | Could not open the ${args[0]}!`)
     }
