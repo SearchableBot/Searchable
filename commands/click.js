b177fadbab79c0949a70526d59457fa4221c1260
Dave <45368841+DaveFeed@users.noreply.github.com>
2021-08-21 16:34:21 +0400

    Create click.js
    Sugondeez balls

---
+++ commands/click.js
@@ -0,0 +1,12 @@
+exports.run = async (client, message, args) => {
+  if(!args[0] || !args[1] || !isNaN(args[0]) || !isNaN(args[1]))
+    return message.channel.send("Proper format: pc!click {X} {Y}");
+  let x = Math.floor(args[0]), y = Math.floor(args[1]);
+  if(x < 0 || y < 0)
+    return message.channel.send("Value has to be grater than 0");
+  if(x > 1920)
+    return message.channel.send("X coordinate is bigger than the window size. Current window size: 1920x1080");
+  if(y > 1080)
+    return message.channel.send("Y coordinate is bigger than the window size. Current window size: 1920x1080");
+  await page.mouse.click(X, Y); //You can add left or right click too, but fuck this im out
+}
