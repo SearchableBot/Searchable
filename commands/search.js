bf52f0505c10de067b56eab2c63adc10653b836a
Dave <45368841+DaveFeed@users.noreply.github.com>
2021-08-21 17:18:13 +0400

    pls work

--- commands/search.js
+++ commands/search.js
@@ -4,11 +4,9 @@ exports.run = async (client, message, args) => {
         return message.channel.send("This link is blacklisted!") 
     try {
         args[0] = args[0].substr(0,4)=="http"?args[0]: `http://${args[0]}`
-        if(/^(ftp|https?):\/\/+(www\.)?[a-z0-9\-\.]{3,}\.[a-z]{3}$/.test(learnRegExp.arguments[0]))
-            return m.edit(":x: | This url is not valid");
         await page.goto(args[0])
-        m.edit(`:x: Opened (this url)[${args[0]}]`)
+        m.edit(`:x: Opened [this url](${args[0]})`)
     } catch (e) {
-        m.edit(`:x: | Could not open the (url)[${args[0]}]!`)
+        m.edit(`:x: | Could not open the [url](${args[0]})!`)
     }
 }
