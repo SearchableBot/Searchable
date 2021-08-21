8588c561b025e9dea0e3db53e9c4b4bbc0c94c52
Dave <45368841+DaveFeed@users.noreply.github.com>
2021-08-21 16:37:00 +0400

    Update click.js
    im a retartd

--- commands/click.js
+++ commands/click.js
@@ -1,5 +1,5 @@
 exports.run = async (client, message, args) => {
-  if(!args[0] || !args[1] || !isNaN(args[0]) || !isNaN(args[1]))
+  if(!args[0] || !args[1] || isNaN(args[0]) || isNaN(args[1]))
     return message.channel.send("Proper format: pc!click {X} {Y}");
   let x = Math.floor(args[0]), y = Math.floor(args[1]);
   if(x < 0 || y < 0)
